efedc80553c6bfe438ad6988a7f04e95e4d1efab
Anton Eklöf <anton_eklof@hotmail.com>
2017-12-11 15:53:25 +0100

    Uppgraderat så att index gunferar

---
+++ MountaiNClimberAI/DSA_Inlupp2_Starter/main.py
@@ -0,0 +1,26 @@
+
+import pygame
+import sys
+import random
+
+
+from map import Map
+from visualiser import Visualiser
+from pathfinder import Pathfinder
+
+x = 0
+map = Map()
+map.loadData()
+
+visualiser = Visualiser( "Inlupp 2 - Visualiser", map.getWidth(), map.getHeight() )
+visualiser.setMap( map )
+
+pathfinder = Pathfinder( visualiser, map )
+
+
+pathfinder.findCheapestPath()
+
+
+visualiser.runLoop()
+
+
